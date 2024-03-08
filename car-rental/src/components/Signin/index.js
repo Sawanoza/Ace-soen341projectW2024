1ef088a39b6019c6864ba78f0cc7bfe066cda2b9
Sawanoza <marc_a00@hotmail.com>
2024-03-08 07:22:54 -0500

    Start of US-4
    Development of landing page, navbar, json scripts, components...

---
+++ car-rental/src/components/Signin/index.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    function handleSubmit(event) {
+        event.preventDefault();
+        axios.post('http://localhost:8081/login', {email, password})
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
+    }
+
+    return (
+        <div>
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor='email'>Email</label>
+                        <input type='email' placeholder='Enter email' onChange={e => setEmail(e.target.value)}/>
+                    </div>
+                    <div>
+                        <label htmlFor='password'>Password</label>
+                        <input type='password' placeholder='Enter password' onChange={e => setPassword(e.target.value)}/>
+                    </div>
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
+
+
+
+
+
+
+
+// import React from 'react'
+// import {Container,
+//         FormWrap,
+//         Icon,
+//         FormContent, 
+//         Form,
+//         FormH1,
+//         FormLabel,
+//         FormInput,
+//         FormButton,
+//         Text
+//     } from './SigninElements'
+
+// const SignIn = () => {
+//   return (
+//     <>
+//     <Container>
+//         <FormWrap>
+//             <Icon to="/">Rent.</Icon>
+//             <FormContent>
+//                 <Form action="#">
+//                     <FormH1>Sign in to your account</FormH1>
+//                     <FormLabel htmlFor='for'>Email</FormLabel>
+//                     <FormInput type='email' required />
+//                     <FormLabel htmlFor='for'>Password</FormLabel>
+//                     <FormInput type='password' required />
+//                     <FormButton type='submit'>Continue</FormButton>
+//                     <Text>Forgot password</Text>
+//                 </Form>
+//             </FormContent>
+//         </FormWrap>
+//     </Container>
+//     </>
+//   )
+// }
+
+// export default SignIn
