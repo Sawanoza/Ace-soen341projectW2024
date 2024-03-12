c93f2bdd32bf089cae0c919a698d1f10ca3722ca
Rach-8 <96176419+Rach-8@users.noreply.github.com>
2024-03-11 21:23:13 -0400

    Update unittest.test.js
    added delete tests

--- __tests__/unittest.test.js
+++ __tests__/unittest.test.js
@@ -96,3 +96,30 @@ describe("POST /reservations", () => {
 
   });
 });
+
+
+
+describe('DELETE /Users/:UserID', () => {
+  it('Successful user deletion', async () => {
+    const res = await request(app).delete('/Users/'); //Change the ID with an existing user.
+    expect(res.status).toBe(200); 
+    expect(res.text).toBe('User deleted successfully');
+  });
+});
+
+describe('DELETE /Vehicles/:VehicleID', () => {
+  it('Successful vehicle deletion', async () => {
+    const res = await request(app).delete('/Vehicles/10'); //Change the ID with an existing vehicle.
+    expect(res.status).toBe(200); 
+    expect(res.text).toBe('Vehicle deleted successfully');
+  });
+});
+
+describe('DELETE /HasReserved/:UserID/:VehicleID', () => {
+  it('should cancel reservation if found', async () => {
+    const res = await request(app)
+      .delete('/HasReserved/3/13'); //Change the IDs with existing ones.
+    expect(res.status).toBe(200); 
+    expect(res.text).toBe('Reservation cancelled successfully');
+  });
+});
