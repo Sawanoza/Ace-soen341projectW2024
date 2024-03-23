4cce011cc1e9ab9fee8c27839ccd425d0da8558e
anassx20 <anass.ta20@gmail.com>
2024-03-23 08:58:11 -0400

    Added_Branches

--- Backend/app.js
+++ Backend/app.js
@@ -82,6 +82,28 @@ app.get("/vehicles", (req, res) => {
   });
 });
 
+app.get('/vehicles/:vehicleID', (req, res) => {
+  const vehicleID = req.params.vehicleID; // Retrieve the vehicle ID from the URL parameters
+
+  // Query the database to get vehicle details based on the ID
+  db.query('SELECT * FROM Vehicles WHERE VehicleID = ?', [vehicleID], (err, results) => {
+      if (err) {
+          console.error('Error retrieving vehicle details:', err);
+          res.status(500).json({ error: 'Internal Server Error' });
+          return;
+      }
+
+      // If no results are found, return a 404 error
+      if (results.length === 0) {
+          res.status(404).json({ error: 'Vehicle not found' });
+          return;
+      }
+
+      // If vehicle details are found, return them as JSON
+      res.json(results[0]);
+  });
+});
+
 app.get("/create_vehicles", (req, res) => {
   res.sendFile(path.join(__dirname, "public/create_vehicles.html"));
 });
@@ -152,15 +174,14 @@ app.post("/item", function (req, res) {
   ]);
 
   const q =
-    "INSERT INTO Vehicles (VehicleID, Brand, Price, Name, Mileage, Images, Seats, Type, IsAvailable) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
+    "INSERT INTO Vehicles (VehicleID, Brand, Price, Name, Mileage, Seats, Type, IsAvailable) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+  
   const values = [
     req.body.vehicleId,
     req.body.brand,
     req.body.price,
     req.body.name,
     req.body.mileage,
-    req.body.images,
     req.body.seats,
     req.body.type,
     req.body.isAvailable,
@@ -171,50 +192,69 @@ app.post("/item", function (req, res) {
       console.error(err);
       return res.send(err);
     }
-    res.redirect("/read_vehicles");
+    
+    // After inserting the vehicle into the Vehicles table, assign it to branch 1 by default
+    const assignToBranchQuery = "INSERT INTO BranchVehicles (BranchID, VehicleID) VALUES (?, ?)";
+    const branchId = 1; // Assuming branch 1 is the default branch
+    const vehicleId = req.body.vehicleId;
+    
+    db.query(assignToBranchQuery, [branchId, vehicleId], (assignErr, assignData) => {
+      if (assignErr) {
+        console.error("Error assigning vehicle to branch:", assignErr);
+        return res.send(assignErr);
+      }
+      res.redirect("/read_vehicles");
+    });
   });
 });
+
 
 // ___________ ADDING DELETE  ___________
 
 //Delete vehicles
+function deleteVehicleById(id, callback) {
+  const deleteBranchVehiclesQuery = `DELETE FROM BranchVehicles WHERE VehicleID = ?`;
+  const deleteVehicleQuery = `DELETE FROM Vehicles WHERE VehicleID = ?`;
 
-function deleteVehiculeById(id, callback) {
-  const query = `DELETE FROM Car_Rental.Vehicles WHERE Vehicleid = ?`;
-  db.query(query, [id], (error, results) => {
+  // First, delete corresponding rows from BranchVehicles table
+  db.query(deleteBranchVehiclesQuery, [id], (error, result) => {
     if (error) {
       callback(error, null);
       return;
     }
-    callback(null, results);
+
+    // Then, delete the vehicle from the Vehicles table
+    db.query(deleteVehicleQuery, [id], (error, result) => {
+      if (error) {
+        callback(error, null);
+        return;
+      }
+      callback(null, result);
+    });
   });
 }
 
+// Route to handle DELETE requests for deleting a vehicle by ID
 app.delete("/Vehicles/:VehicleID", (req, res) => {
-  const idToDelete = req.params.VehicleID; 
-  deleteVehiculeById(idToDelete, (error, results) => {
+  const idToDelete = req.params.VehicleID;
+  deleteVehicleById(idToDelete, (error, results) => {
     if (error) {
       console.error('Error deleting vehicle by ID:', error);
-      res.status(500).send('Internal Server Error'); 
+      res.status(500).send('Internal Server Error');
       return;
     }
     if (results.affectedRows === 0) {
-      res.status(404).send('Vehicle not found'); 
+      res.status(404).send('Vehicle not found');
     } else {
-      res.status(200).send('Vehicle deleted successfully'); 
+      res.status(200).send('Vehicle deleted successfully');
     }
   });
 });
 
-//Delete users
-
 // Function to delete a user by ID from the database
-
-// Important note, when a user is deleted by ID from "Users", the associated records in "HasReserved" should be deleted
-
 function deleteUserByIdWithAssociatedRecords(id, callback) {
-  const deleteHasReservedQuery = `DELETE FROM Car_Rental.HasReserved WHERE UserID = ?`;
-  const deleteUserQuery = `DELETE FROM Car_Rental.Users WHERE UserID = ?`;
+  const deleteHasReservedQuery = `DELETE FROM HasReserved WHERE UserID = ?`;
+  const deleteUserQuery = `DELETE FROM Users WHERE UserID = ?`;
 
   db.query(deleteHasReservedQuery, [id], (error, result) => {
     if (error) {
@@ -231,7 +271,7 @@ function deleteUserByIdWithAssociatedRecords(id, callback) {
   });
 }
 
-
+// Route to handle DELETE requests for deleting a user by ID
 app.delete("/Users/:UserID", (req, res) => {
   const idToDelete = req.params.UserID;
   deleteUserByIdWithAssociatedRecords(idToDelete, (error, results) => {
@@ -355,24 +395,214 @@ app.put("/Users/:UserID", (req, res) => {
 
 //_______ END OF UPDATE _________
 
-//_______ START SEACH ___________
+//_______ START SEARCH ___________
 
+// Branch Search
 app.get('/search', (req, res) => {
   const searchTerm = req.query.term;
-  // Perform a database query to find vehicles matching the search term
-  const query = `SELECT * FROM Vehicles WHERE brand LIKE '%${searchTerm}%' OR name LIKE '%${searchTerm}%'`;
-  db.query(query, (err, results) => {
-    if (err) {
-      console.error('Error searching for vehicles:', err);
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
-    }
-    res.json(results); // Send the search results back to the client
-  });
+  if (!isNaN(searchTerm)) {
+    const query = `SELECT * FROM Branch WHERE id = ${searchTerm}`;
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error('Error searching for branch by ID:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+      res.json(results); 
+    });
+  } else {
+    const query = `SELECT * FROM Branch WHERE location LIKE '${searchTerm}%'`;
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error('Error searching for branch by location:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+      res.json(results); 
+    });
+  }
 });
+
+// Vehicles search
+app.get('/search/vehicles', (req, res) => {
+  const searchTerm = req.query.term;
+
+  if (!isNaN(searchTerm)) {
+    // If the search term is a number, search by VehicleID
+    const query = `SELECT * FROM Vehicles WHERE VehicleID = ${searchTerm}`;
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error('Error searching for vehicle by ID:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+      res.json(results); 
+    });
+  } else {
+    // If the search term is not a number, search by Brand, Name, or Type
+    const query = `
+      SELECT * FROM Vehicles 
+      WHERE Brand LIKE '%${searchTerm}%' 
+      OR Name LIKE '%${searchTerm}%' 
+      OR Type LIKE '%${searchTerm}%'
+    `;
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error('Error searching for vehicles by brand, name, or type:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+        return;
+      }
+      res.json(results); 
+    });
+  }
+});
+
 
 // _______ END OF SEARCH _________
 
+// Route to fetch all branches
+app.get("/branches", (req, res) => {
+  const q = "SELECT * FROM Branch";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
 
+// Route to fetch branch details by ID along with associated vehicles
+app.get("/branches/:branchId", (req, res) => {
+  const branchId = req.params.branchId;
+  const branchQuery = "SELECT * FROM Branch WHERE id = ?";
+  const vehiclesQuery = "SELECT * FROM Vehicles INNER JOIN BranchVehicles ON Vehicles.VehicleID = BranchVehicles.VehicleID WHERE BranchVehicles.BranchID = ?";
+  
+  db.query(branchQuery, [branchId], (err, branchData) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+    if (branchData.length === 0) {
+      return res.status(404).json({ error: "Branch not found" });
+    }
+
+    db.query(vehiclesQuery, [branchId], (err, vehiclesData) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Internal Server Error" });
+      }
+
+      const branchDetails = branchData[0];
+      branchDetails.vehicles = vehiclesData; 
+      res.json(branchDetails);
+    });
+  });
+});
+
+// Route to get branch details by ID
+app.get("/branches/details/:branchId", (req, res) => {
+  const branchId = req.params.branchId;
+  const branchQuery = "SELECT * FROM Branch WHERE id = ?";
+  
+  db.query(branchQuery, [branchId], (err, branchData) => {
+      if (err) {
+          console.error(err);
+          return res.status(500).json({ error: "Internal Server Error" });
+      }
+      if (branchData.length === 0) {
+          return res.status(404).json({ error: "Branch not found" });
+      }
+      
+      const branchDetails = branchData[0];
+      res.json(branchDetails);
+  });
+});
+
+// Route to create a new branch
+app.post("/branches", function (req, res) {
+  const q =
+    "INSERT INTO Branch (id, name, location) VALUES (?, ?, ?)";
+  const values = [
+    req.body.branchId,
+    req.body.name,
+    req.body.location
+  ];
+  db.query(q, values, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.send(err);
+    }
+    res.redirect("/branches");
+  });
+});
+
+// Route to fetch all vehicles assigned to a specific branch
+app.get("/branches/:branchId/vehicles", (req, res) => {
+  const branchId = req.params.branchId;
+  const q = "SELECT * FROM BranchVehicles WHERE BranchID = ?";
+  db.query(q, [branchId], (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+// Route to assign a vehicle to a branch
+app.post("/branches/:branchId/vehicles", function (req, res) {
+  const q =
+    "INSERT INTO BranchVehicles (BranchID, VehicleID) VALUES (?, ?)";
+  const values = [
+    req.params.branchId,
+    req.body.vehicleId
+  ];
+  db.query(q, values, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.send(err);
+    }
+    res.redirect(`/branches/${req.params.branchId}/vehicles`);
+  });
+});
+
+
+// Route to delete a vehicle assignment from a branch
+app.delete("/branches/:branchId/vehicles/:vehicleId", (req, res) => {
+  const branchId = req.params.branchId;
+  const vehicleId = req.params.vehicleId;
+  const q = "DELETE FROM BranchVehicles WHERE BranchID = ? AND VehicleID = ?";
+  db.query(q, [branchId, vehicleId], (error, results) => {
+    if (error) {
+      console.error('Error deleting vehicle assignment:', error);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    if (results.affectedRows === 0) {
+      res.status(404).send('Vehicle assignment not found');
+    } else {
+      res.status(200).send('Vehicle assignment deleted successfully');
+    }
+  });
+});
+
+
+// Route to get the branch based on the vehicle ID
+app.get("/vehicles/:vehicleId/branch", (req, res) => {
+  const vehicleId = req.params.vehicleId;
+  const q = "SELECT BranchID FROM BranchVehicles WHERE VehicleID = ?";
+  db.query(q, [vehicleId], (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+    if (data.length === 0) {
+      return res.status(404).json({ error: "Vehicle not found in any branch" });
+    }
+    const branchId = data[0].BranchID;
+    return res.json({ BranchID: branchId });
+  });
+});
 
 
